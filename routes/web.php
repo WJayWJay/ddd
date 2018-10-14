cdb82e98a53c2a176c96452b0b5295be9c243df6
hellokitty <wj342234130@gmail.com>
2018-10-15 07:52:58 +0800

    init project

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/api/user', ['middleware'=> 'auth', function (Request $request) use ($router) {
+    return response('aaa');
+}]);
+
+
+$router->group(['middleware' => 'auth', 'prefix' => 'api'], function () use ($router) {
+    $router->get('users', function () {
+        return response('hello world');
+    });
+});
