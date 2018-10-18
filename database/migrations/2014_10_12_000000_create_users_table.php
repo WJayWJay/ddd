9661d37b5b8db5c6e4add1a625f5a316b54a0452
hellokitty <wujian@followme-inc.com>
2018-10-18 17:51:44 +0800

    feat: init project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->smallInteger('isActived')->comment('是否激活: 1 激活, 2 未激活')->default(2);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', 'HomeController@test');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
